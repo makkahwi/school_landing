3529c234a20f5ac5d76403ec770dbfc888c715ec
MustafaHasanat <mustafa.hasanat99@gmail.com>
2023-02-15 15:36:51 +0300

    fixing deployment isuue 5

--- pages/styles.ts
+++
@@ -1,9 +0,0 @@
-import { Container, Stack } from "@mui/material";
-import { keyframes, styled } from "@mui/material/styles";
-import CSS from "csstype";
-import Avatar from "@mui/material/Avatar";
-import { ReactNode } from "react";
-
-// Landing page components ------------------------------------------
-
-
