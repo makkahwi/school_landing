14ade050c1c620cbdcdb541e74003cb11e8dbc59
MustafaHasanat <mustafa.hasanat99@gmail.com>
2023-03-12 21:24:41 +0300

    finishing the about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,11 +1,14 @@
 import WhyAISSection from "@/components/aboutPage/whyAISSection";
+import WelcomeSection from "@/components/aboutPage/welcomeSection";
+import AISBeginning from "@/components/aboutPage/aisBeginning";
 import { Stack } from "@mui/material";
-
 
 export default function AboutAIS() {
     return (
         <Stack>
+            <WelcomeSection />
             <WhyAISSection />
+            <AISBeginning />
         </Stack>
     );
 }
