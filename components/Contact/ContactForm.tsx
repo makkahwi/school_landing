3a0f9d4365d7ba5bdda7eb0efd79865d9c2a8c38
makkahwi <suhaibahmadai@hotmail.com>
2023-09-22 19:44:56 +0300

    Minor UI Update

--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -16,43 +16,49 @@ const ContactForm = () => {
   const { t } = useTranslation(router);
 
   const formInput = [
-    { title: t(`contactPage.formInput1`), name: "name" },
-    { title: t(`contactPage.formInput2`), name: "email" },
-    { title: t(`contactPage.formInput3`), name: "phone" },
-    { title: t(`contactPage.formInput4`), name: "age" },
-    { title: t(`contactPage.formInput5`), name: "msg", fullWidth: true },
+    { title: t(`contactPage.formInput1`), name: "name", required: true },
+    {
+      title: t(`contactPage.formInput2`),
+      name: "email",
+      type: "email",
+      required: true,
+    },
+    {
+      title: t(`contactPage.formInput3`),
+      name: "phone",
+      type: "number",
+      required: true,
+    },
+    { title: t(`contactPage.formInput4`), name: "age", type: "number" },
+    {
+      title: t(`contactPage.formInput5`),
+      name: "msg",
+      required: true,
+      multiline: true,
+      rows: 3,
+    },
   ];
 
   return (
     <PageSection>
       <PageSectionColumn>
-        <CardComp bg={theme.palette.basic.main}>
-          <Text color={theme.palette.brown.main} variant="subtitle" center>
-            {t("contactPage.formTitle")}
-          </Text>
+        <Text color={theme.palette.brown.main} variant="subtitle" center>
+          {t("contactPage.formTitle")}
+        </Text>
 
-          <Row py={5} px={20}>
-            {formInput.map(({ title, fullWidth }, i) => (
-              <Column md={fullWidth ? 12 : 6} p={1} key={i}>
-                <Text color={theme.palette.brown.main} bold>
-                  {title}
-                </Text>
-
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  style={{ margin: "10px auto", backgroundColor: "white" }}
-                />
-              </Column>
-            ))}
-
-            <Column md={12} pt={5}>
-              <Button size="large" fullWidth color="info" variant="contained">
-                {t("contactPage.formButton")}
-              </Button>
+        <Row py={5} px={20}>
+          {formInput.map(({ title, ...rest }, i) => (
+            <Column md={12} py={2} key={i}>
+              <TextField variant="filled" label={title} fullWidth {...rest} />
             </Column>
-          </Row>
-        </CardComp>
+          ))}
+
+          <Column md={12} pt={5}>
+            <Button size="large" fullWidth color="info" variant="contained">
+              {t("contactPage.formButton")}
+            </Button>
+          </Column>
+        </Row>
       </PageSectionColumn>
     </PageSection>
   );
