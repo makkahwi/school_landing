bb1ed1edc4c78660f372f98a8018cecedd858ac5
makkahwi <suhaibahmadai@hotmail.com>
2023-09-27 18:14:48 +0300

    Add Initial Suspense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
 import "@/styles/globals.css";
 
-import type { AppProps } from "next/app";
 import Layout from "@/Layout";
+import Loading from "@/Layout/Loading";
 import theme from "@/styles/theme";
 import { ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+import { Suspense } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Suspense fallback={<Loading />}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Suspense>
     </ThemeProvider>
   );
 }
