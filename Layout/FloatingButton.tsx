bf1449fa80d88d3e37e6191261e7d7016f8b02f4
Makkahwi <SuhaibAhmadAi@hotmail.com>
2025-04-09 16:16:11 +0300

    Fix Floating Button UI & Func

--- Layout/FloatingButton.tsx
+++ Layout/FloatingButton.tsx
@@ -2,16 +2,27 @@ import { WhatsApp } from "@mui/icons-material";
 import { Box, Button, styled } from "@mui/material";
 
 const OuterWrapper = styled(Box)(() => ({
-  position: "absolute",
+  position: "fixed",
   right: 0,
   bottom: 0,
-  margin: 3,
+  margin: 4,
 }));
 
 const FloatingButton = () => {
   return (
     <OuterWrapper>
-      <Button size="small" color="success" href="http://wasap.my/601128884817">
+      <Button
+        size="small"
+        color="white"
+        sx={{
+          width: "content-fit",
+          bgcolor: "#25d366",
+          color: "white",
+          margin: 3,
+        }}
+        target="_blank"
+        href="http://wasap.my/601128884817"
+      >
         <WhatsApp />
       </Button>
     </OuterWrapper>
